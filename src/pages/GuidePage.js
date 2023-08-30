52cf1de8fa3a8318a5518e7d57b8d61a16fd599d
songjuyoung-1410 <chicken010208@gmail.com>
2023-08-30 09:54:24 +0900

    guidePage 구조 수정

--- src/pages/GuidePage.js
+++ src/pages/GuidePage.js
@@ -1,8 +1,10 @@
 import Header from "../components/Header";
 import "../styles/guidePage.css";
+import { ReactComponent as Rabbits } from "../images/guidePage/rabbits.svg";
 import { ReactComponent as Content1 } from "../images/guidePage/content1.svg";
 import { ReactComponent as Content2 } from "../images/guidePage/content2.svg";
 import { ReactComponent as Content3 } from "../images/guidePage/content3.svg";
+import { ReactComponent as Contents } from "../images/guidePage/contents.svg";
 import { ReactComponent as ToContent } from "../images/guidePage/toContent.svg";
 
 export default function GuidePage() {
@@ -12,20 +14,28 @@ export default function GuidePage() {
       <div className="guide-header">
         <Header />
       </div>
-      <div className="guide-background">
+      <div className="guide-background1">
+        <div className="rabbits">
+          <Rabbits />
+        </div>
         <div>
-          <div className="content1" onClick={() => window.scrollTo(0, 800)}>
+          <div className="content1" onClick={() => window.scrollTo(0, 740)}>
             <Content1 />
           </div>
-          <div className="content2" onClick={() => window.scrollTo(0, 1530)}>
+          <div className="content2" onClick={() => window.scrollTo(0, 1430)}>
             <Content2 />
           </div>
-          <div className="content3" onClick={() => window.scrollTo(0, 2560)}>
+          <div className="content3" onClick={() => window.scrollTo(0, 2410)}>
             <Content3 />
           </div>
         </div>
+        <p className="text-content">CONTENTS</p>
       </div>
-      <div></div>
+      <div className="guide-background2">
+        <div className="contents">
+          <Contents />
+        </div>
+      </div>
       <div
         onClick={() => {
           window.scrollTo({
